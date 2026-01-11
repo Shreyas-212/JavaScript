1f8c11dc8af4824a064b65e07f530b4124ecd5ee
Shreyas_212 <shreyasthoke2104@gmail.com>
2026-01-11 22:26:41 +0530

    adding operators code

---
+++ operators.js
@@ -0,0 +1,42 @@
+//Arithmetic Operators
+let a = 5;
+let b = 10;
+
+console.log("a is ", a, " b is ", b);
+console.log("a + b = ", a + b);
+console.log("a - b = ", a - b);
+console.log("a * b = ", a * b);
+console.log("a / b = ", a / b);
+
+//Modulus Operator
+console.log(" a % b = ", a % b);
+
+//Exponentiation Operator
+console.log("a ** b = ", a ** b);
+
+//Unary Operators - Increment and Decrement 
+console.log("a is ", a, " b is ", b);
+console.log("a = ", a++);
+console.log("b = ", b--);
+
+//Assignment Operators
+console.log("a + 4 = ", a += 4);
+console.log("a - 4 = ", a -= 4);
+console.log("a * 4 = ", a *= 4);
+console.log("a / 4 = ", a /= 4);
+console.log("a % 4 = ", a %= 4);
+console.log("a ** 4 = ", a **= 4);
+
+//Comparison Operators - ==, !=, ===, !==, >, >=, <, <=
+console.log("a == b ", a == b);
+console.log("a != b ", a != b);
+
+//Logical Operators - &&, ||, !
+let c = 6;
+let d = 5;
+let cond1 = c > d;
+let cond2 = d === 5;
+let cond3 = a === 3;
+console.log("cond1 && cond2 = ", cond1 && cond2);
+console.log("cond1 || condi3 = ", cond1 || cond3);
+console.log("!cond1 = ", !cond1);
