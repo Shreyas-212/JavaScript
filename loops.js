c6ac1251f612b905ba650d67bb682e47c0ffbca3
Shreyas_212 <shreyasthoke2104@gmail.com>
2026-01-11 22:28:48 +0530

    adding loops code

---
+++ loops.js
@@ -0,0 +1,23 @@
+//for loop
+for (let i=0; i<5; i++) {
+    console.log("Hello");
+}
+
+//while loop
+let i = 1;
+while (i <= 5) {
+    console.log(i);
+    i++;
+}
+
+//do while loop
+do{
+    console.log(i);
+    i++;
+} while (i <= 10)
+
+//for of loop or for in loop
+let str = "Tony Stark"
+for (let i of str) {
+    console.log(i);
+}
