6c883a5175f55fed9402119280729b867d309d33
Shreyas_212 <shreyasthoke2104@gmail.com>
2026-01-13 14:02:09 +0530

    adding practice problem code

---
+++ practiceProblem9.js
@@ -0,0 +1,25 @@
+function countVowels(str) {
+    let count = 0;
+    for(const char of str) {
+        if(char === "a" || char === "e" || char === "i" || char === "o" || char === "u") {
+            count++;
+        }
+    }
+
+    console.log(count);
+}
+
+countVowels("Hello World ");
+
+const countVow = (str) => {
+    let count = 0;
+    for(const char of str) {
+        if(char === "a" || char === "e" || char === "i" || char === "o" || char === "u") {
+            count++;
+        }
+    }
+    console.log(count);
+}
+
+countVow("aeiou");
+
